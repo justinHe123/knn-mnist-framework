4e921c71adc1f8c03c361aadc11e7207d2395894
justinHe123 <justinme100@gmail.com>
2019-02-11 09:37:12 -0800

    implemented cosine similarity

--- src/Classifier.java
+++ src/Classifier.java
@@ -29,15 +29,51 @@ public class Classifier {
         addTrainingData(new DataPoint(label, img));
     }
 
-    public String classify(short[][] pixels) {
-        if (trainingData.size() == 0) return "no training data";
+//    public String classify(short[][] pixels) {
+//        if (trainingData.size() == 0) return "no training data";
+//
+//        // TODO: write a k-nearest-neighbor classifier.  Return its prediction of "0" to "9"
+//        DataPoint closestPoint = null;
+//
+//        ArrayList<DataPoint> closePoints = new ArrayList<DataPoint>();
+//        int integer = 0;
+//        int furthestIndex = 0;
+//        for (DataPoint point : trainingData){
+//            if(closePoints.size() < n){
+//                closePoints.add(point);
+//            } else {
+//                if (integer == 0) {
+//                    furthestIndex = furthestIndex(closePoints, pixels);
+//                    integer++;
+//                }
+//                short[][] d1 = closePoints.get(furthestIndex).getData().getBWPixelGrid();
+//                short[][] d2 = point.getData().getBWPixelGrid();
+//
+//                if (distance(d2, pixels) < distance(d1, pixels)){
+//                    closePoints.remove(furthestIndex);
+//                    closePoints.add(point);
+//                    integer--;
+//                }
+//            }
+//        }
+//        return mostFrequentLabel(closePoints);
+//    }
 
+    public String classify(short[][] pixels){
+        if (trainingData.size() == 0) return "no training data";
         // TODO: write a k-nearest-neighbor classifier.  Return its prediction of "0" to "9"
         DataPoint closestPoint = null;
-
         ArrayList<DataPoint> closePoints = new ArrayList<DataPoint>();
         int integer = 0;
         int furthestIndex = 0;
+        double a = 0;
+        for (short[] shorts : pixels){
+            for (short val : shorts){
+                a += (double)(val*val);
+            }
+        }
+        a = Math.sqrt(a);
+
         for (DataPoint point : trainingData){
             if(closePoints.size() < n){
                 closePoints.add(point);
@@ -49,7 +85,7 @@ public class Classifier {
                 short[][] d1 = closePoints.get(furthestIndex).getData().getBWPixelGrid();
                 short[][] d2 = point.getData().getBWPixelGrid();
 
-                if (distance(d2, pixels) < distance(d1, pixels)){
+                if (cossim(a, pixels, d2) < cossim(a, pixels, d1)){
                     closePoints.remove(furthestIndex);
                     closePoints.add(point);
                     integer--;
@@ -57,6 +93,29 @@ public class Classifier {
             }
         }
         return mostFrequentLabel(closePoints);
+    }
+
+    public double cossim(double a, short[][] dA, short[][] dB){ //a = ||A||
+        short[] arrA = new short[784];
+        short[] arrB = new short[784];
+
+        for (int r = 0; r < dA.length; r++){
+            for (int c = 0; c < dA[r].length; c++){
+                arrA[r*dA[r].length + c] = dA[r][c];
+                arrB[r*dB[r].length + c] = dB[r][c];
+            }
+        }
+
+        double b = 0;
+        double ab = 0;
+        for (int i = 0; i < arrA.length; i++){
+            b += (double)(arrB[i] * arrB[i]);
+            ab += (double)(arrA[i] * arrB[i]);
+        }
+        b = Math.sqrt(b);
+
+        double cos = ab / (a * b);
+        return 1.0 - cos;
     }
 
     public String mostFrequentLabel(ArrayList<DataPoint> points){
